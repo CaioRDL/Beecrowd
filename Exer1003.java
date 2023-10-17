76d7bf2298d22e7c3bd8cfb99b121cc770d85c4b
Caio Ramos de Lima <ramoscaio69@gmail.com>
2023-10-16 23:25:43 -0300

    Update Exer1003.java

--- Exer1003.java
+++ Exer1003.java
@@ -1,1 +1,25 @@
-  
+import java.util.Scanner;
+
+public class exer1003 {
+
+    
+    public static void main (String[] args){
+
+    Scanner read = new Scanner(System.in) ;
+		
+    System.out.println("Digite o valor de A: ");
+    int A = read.nextInt();
+
+    System.out.println("Digite o valor de B: ");
+    int B = read.nextInt();
+    
+    
+    int SOMA = A + B;
+    
+    System.out.println("SOMA = " + SOMA);
+
+
+    read.close();
+    
+}
+}
